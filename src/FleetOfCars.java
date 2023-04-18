b1b0992b091c9426f6be006b9a64bafc050f7c3d
lassekh <lasse.kh.92@gmail.com>
2023-04-18 21:35:40 +0200

    Main and FleetOfCars

--- src/FleetOfCars.java
+++ src/FleetOfCars.java
@@ -2,13 +2,27 @@ import java.util.ArrayList;
 
 public class FleetOfCars {
     private ArrayList<Car> fleet;
-    void addCar(Car car){};
+    public FleetOfCars(){
+        this.fleet = new ArrayList<>();
+    }
+    public void addCar(Car car){
+        this.fleet.add(car);
+    }
+
+    public ArrayList<Car> getFleet() {
+        return fleet;
+    }
+
     public int getTotalRegistrationFeeForFleet() {
-        int total = 0;
+        int total =0;
         return total;
     }
     @Override
     public String toString(){
+        String car;
+        for(int i = 0; i < fleet.size(); i++){
+            //car = fleet.get(i.);
+        }
         return "";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        FleetOfCars LassesCarRental = new FleetOfCars();
+        Car electricCar = new ElectricCar("SD20564", "Volkswagen","ID.5",5,1000,1500);
+        Car gasolinCar = new GasolinCar("JH33198","Skoda", "Fabia",5,15);
+        Car dieselCar = new DieselCar("UI45444","Peugeot", "308",5,27,true);
+        LassesCarRental.addCar(electricCar);
+        LassesCarRental.addCar(gasolinCar);
+        LassesCarRental.addCar(dieselCar);
     }
 }
